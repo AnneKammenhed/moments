2d17b0eca6e515ab22fe42a5a2fb43dd89667f14
AnneKammenhed <anne.juriado@gmail.com>
2022-10-31 12:01:46 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import NavBar from './components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hej värld!</h1>
+      <NavBar />
     </div>
   );
 }
